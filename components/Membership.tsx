1b44a3b426865c957e5b2de0c5cc88c196623f9e
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-26 16:45:34 +0530

    Updated Membership with more fields

--- components/Membership.tsx
+++ components/Membership.tsx
@@ -2,6 +2,7 @@ import Loader from "./Loader";
 import { useState } from "react";
 import useAuth from "../hooks/useAuth";
 import useSubscription from "../hooks/useSubscription";
+import { goToBillingPortal } from "../lib/stripe";
 
 function Membership() {
 	const { user } = useAuth();
@@ -11,7 +12,7 @@ function Membership() {
 	const manageSubscription = () => {
 		if (subscription) {
 			setBillingLoading(true);
-			// goToBillingPortal();
+			 goToBillingPortal();
 		}
 	};
 	return (
@@ -29,6 +30,35 @@ function Membership() {
 						"Cancel Membership"
 					)}
 				</button>
+			</div>
+			<div className='col-span-3'>
+				<div className='flex flex-col justify-between border-b border-white/10 py-4 md:flex-row'>
+					<div>
+						<p className='font-medium'>{user?.email}</p>
+						<p className='text-[gray]'>Password: ********</p>
+					</div>
+					<div className='md:text-right'>
+						<p className='membershipLink'>Change email</p>
+						<p className='membershipLink'>Change password</p>
+					</div>
+				</div>
+
+				<div className='flex flex-col justify-between pt-4 pb-4 md:flex-row md:pb-0'>
+					<div>
+						<p>
+							{subscription?.cancel_at_period_end
+								? "Your membership will end on "
+								: "Your next billing date is "}
+							{subscription?.current_period_end}
+						</p>
+					</div>
+					<div className='md:text-right'>
+						<p className='membershipLink'>Manage payment info</p>
+						<p className='membershipLink'>Add backup payment method</p>
+						<p className='membershipLink'>Billing Details</p>
+						<p className='membershipLink'>Change billing day</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
