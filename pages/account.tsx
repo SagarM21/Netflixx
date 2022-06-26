ac9673ebe81fb8175a8e03dfdb6545881109ff19
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2022-06-26 15:34:38 +0530

    Account page with signout func

--- pages/account.tsx
+++ pages/account.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 
 function Account({ products }: Props) {
-	const { user } = useAuth();
+	const { user, logout } = useAuth();
 	const subscription = useSubscription(user);
 	return (
 		<div>
@@ -61,6 +61,16 @@ function Account({ products }: Props) {
 					</div>
 					<p className='cursor-pointer text-blue-500 hover:underline md:text-right'>
 						Change Plan
+					</p>
+				</div>
+
+				<div className='mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0'>
+					<h4 className='text-lg text-[gray]'>Settings</h4>
+					<p
+						className='col-span-3 cursor-pointer text-blue-500 hover:underline'
+						onClick={logout}
+					>
+						Sign out of all devices
 					</p>
 				</div>
 			</main>
